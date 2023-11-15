aa5a1b35b70173c8457571528b80462d7d9249f5
Kevin Lavenant <kevinlavenant@MacBook-Pro-de-Kevin.local>
2023-11-15 14:12:16 +0100

    dossiers créés

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet PHP calendrier</title>
+    <!-- my style -->
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+</head>
+<body>
+    Hello
+
+    <!-- my script -->
+    <script src="./public/assets/js/script.js"></script>
+</body>
+</html>
